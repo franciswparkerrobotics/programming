4e82b491b0f8647d04164a7e9dcaf1dde5a73900
Sirawesomeknight <hulsizerjames@gmail.com>
2014-09-23 17:33:57 -0700

    General Power Distribution Protocol Added
    It took awhile, but it's finally done. Check the comments to see how my
    formula works.

---
+++ GPDProtocol.h
@@ -0,0 +1,14 @@
+//General Power Distribution Protocol
+int checkBattery(float powerp){
+float avgbLVL = nAvgBatteryLevel;
+float float totalVwant;
+//converts input to decimal
+powerp = powerp / 100;
+//gets milivoltage out of max number of voltage
+totalVwant = powerp * 12000;
+//gets percent of power needed. milivoltage of max power / milivoltage of power availible;
+powerp = totalVwant / avgbLVL;
+//turns percent back into input
+powerp = power * 100
+return powerp;
+}
