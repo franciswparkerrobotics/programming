fb617e41e0abea7e8bd8fa4df4a81d3587eb3cd2
bernhardtjj <jbernhardt@fwparker.org>
2014-09-24 11:36:29 -0500

    Added Gyro lib
    need to find conversion for gyro into degrees

--- GyroAuto.c
+++ GyroAuto.c
@@ -12,11 +12,12 @@ float initial; float heading; float radheading; int lastTime = 0;//initial is th
 float FLset; float FRset;float gjoy1_x1;float gjoy1_x2;float gjoy1_y1;float gjoy1_y2;// these stand for front right set and front left set, which refer to wheels on the robot
 //FL set is the front-left motor and the back-right motor, and FR set is the other two motors
 float joyAngle; // angle of the first joystick
-void setJoysticks(float joy1_x1,float joy1_x2,float joy1_y1,float joy1_y2){
+float setJoysticks(float joy1_x1,float joy1_x2,float joy1_y1,float joy1_y2){
 	gjoy1_x1=joy1_x1;
 	gjoy1_y1=joy1_x2;
 	gjoy1_x2=joy1_y1;
 	gjoy1_y2=joy1_y2;
+	return heading;
 }
 void moveDirection(float angle, float magnitude){ // sets the motor sets to move at certain speeds depending on the vector given
 	FLset = magnitude * (cos(angle) + sin(angle));
