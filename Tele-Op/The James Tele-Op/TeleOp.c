e97df3687497aa4dc42c2fc3eb3e3cd0491fbc4a
Sirawesomeknight <hulsizerjames@gmail.com>
2014-11-04 16:25:36 -0600

    Organization Stuff

--- Tele-Op/The James Tele-Op/TeleOp.c
+++ Tele-Op/The James Tele-Op/TeleOp.c
@@ -119,7 +119,6 @@ task main()
   waitForStart();   // wait for start of tele-op phase
   bFloatDuringInactiveMotorPWM = false;
   StartTask(leftj);
-  //StartTask(rightj);
   while(true){
   	wait1Msec(10);
 
